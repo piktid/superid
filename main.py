ce17a0e597e1bc53c1d8bf1cc578a7ef31fe942d
Nunzio Alexandro Letizia <64853153+nuletizia@users.noreply.github.com>
2023-11-15 10:21:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import requests
+import json
+from io import BytesIO
+from PIL import Image, ImageFile, ImageFilter
+import argparse
+
+from superid_api import up_param, start_call, upload_superid_call, upscaling_call, get_notification_call, get_superid_info, get_superid_link
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--upscaler_type', help='Select which upscaler to use: 0 (None), 1 (People), 2 (Scene), 3 (Mix)', default='1')
+    parser.add_argument('--upscaling_mode', help='Select the upscaling mode: fast (takes few seconds, lower quality) and normal (takes up to minutes, higher quality)', default='fast')
+    parser.add_argument('--scale_factor', help='Select the upscaling factor: upscale time 2 or times 4', default='2')
+    parser.add_argument('--output_format', help='Save the upscaled image in PNG or JPEG', default='PNG')
+    # Parameters only for the normal mode
+    parser.add_argument('--prompt', help='Describe your image', default='')
+    parser.add_argument('--prompt_strength', help='The lower the more similar to the input image, the higher, the more diverse (range 0-1)', default='0.1')
+    parser.add_argument('--controlnet_scale', help='The higher, the more the upscaling will follow the lines of the input image (range 0-1)', default='0.5')
+    parser.add_argument('--num_inference_steps', help='The higher, the more denoising steps', default=20)
+
+    args = parser.parse_args()
+
+    # be sure to export your email and psw as environmental variables
+    EMAIL = os.getenv("SUPERID_EMAIL")
+    PASSWORD = os.getenv("SUPERID_PASSWORD")
+
+    ## START
+    # insert the URL of the image to anonymize
+    url = 'https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png' # photo of Lenna
+    input_img = Image.open(BytesIO(requests.get(url,stream=False).content))
+
+    # start the call
+    TOKEN = start_call(EMAIL, PASSWORD)
+
+    # parameters initialization
+    upscaling_parameters = up_param(args)
+
+    # upload
+    id_project, id_image = upload_superid_call(input_img, TOKEN)
+
+    # get an estimate of the amount of credits and time needed
+    eta, credits, width, height = get_superid_info(id_project, id_image, upscaling_parameters, TOKEN)
+    print(f'Image size: {width}, {height}. Estimated time of arrival of upscaled image: {eta}, and estimated credits: {credits}')
+
+    # upscale
+    output = upscaling_call(id_project, id_image, upscaling_parameters, TOKEN)
+    print(f'Upscaling call output: {output}')
+
+
+    # get the notification, if it is there
+    while 1:
+      try:
+        link = get_superid_link(id_project, id_image, TOKEN)
+        print(f'Image available for download at: {link}')
+        break
+      except:
+        # notification is not there yet, wait 10 seconds
+        print(f'Server is either booting up or processing your image, wait some seconds')
+      sleep(10)
+
+    # save output_img as you prefer..
+    # output_img = Image.open(requests.get(link,stream=True).raw)
+
+
---
+++ superid_api.py
@@ -0,0 +1,176 @@
+import os
+import requests
+import json
+from io import BytesIO
+import base64
+from PIL import Image, ImageFile, ImageFilter, ImageCms
+import copy
+import time
+from time import sleep
+from requests_toolbelt import MultipartEncoder
+
+
+URL_API = 'https://api.piktid.com/api'
+
+##----UPSCALING CLASS----
+# Do not modify the parameters unless you are an expert
+class up_param():
+    def __init__(self, args):
+        self.upscaler_type = args.upscaler_type
+        self.upscaling_mode = args.upscaling_mode
+        self.scale_factor = args.scale_factor
+        self.output_format = args.output_format
+        self.prompt = args.prompt
+        self.controlnet_scale = args.controlnet_scale
+        self.prompt_strength = args.prompt_strength
+        self.num_inference_steps = args.num_inference_steps
+
+## -----------READ/WRITE FUNCTIONS------------
+def open_image_from_url(url):
+    response = requests.get(url, stream=True)
+    if not response.ok:
+        print(response)
+
+    image = Image.open(BytesIO(response.content))
+    return image
+
+def open_image_from_url_bytes(url, only_face=False):
+    response = requests.get(url, stream=True)
+    if not response.ok:
+        print(response)
+
+    return BytesIO(response.content)
+
+def im_2_B(image):
+    # Convert Image to buffer
+    buff = BytesIO()
+
+    if image.mode == 'CMYK':
+        image = ImageCms.profileToProfile(image, 'ISOcoated_v2_eci.icc', 'sRGB Color Space Profile.icm', renderingIntent=0, outputMode='RGB')
+
+    image.save(buff, format='PNG',icc_profile=image.info.get('icc_profile'))
+    img_str = buff.getvalue()
+    return img_str
+
+def im_2_buffer(image):
+    # Convert Image to bytes 
+    buff = BytesIO()
+
+    if image.mode == 'CMYK':
+        image = ImageCms.profileToProfile(image, 'ISOcoated_v2_eci.icc', 'sRGB Color Space Profile.icm', renderingIntent=0, outputMode='RGB')
+
+    image.save(buff, format='PNG',icc_profile=image.info.get('icc_profile'))
+    return buff
+
+def b64_2_img(data):
+    # Convert Base64 to Image
+    buff = BytesIO(base64.b64decode(data))
+    return Image.open(buff)
+    
+def im_2_b64(image):
+    # Convert Image 
+    buff = BytesIO()
+    image.save(buff, format='PNG')
+    img_str = base64.b64encode(buff.getvalue()).decode('utf-8')
+    return img_str
+
+
+## -----------PROCESSING FUNCTIONS------------
+def start_call(email, password):
+    # Get token
+    response = requests.post(URL_API+'/tokens', data={}, auth=(email, password))
+    response_json = json.loads(response.text)
+    TOKEN = response_json['access_token']
+
+    return TOKEN
+
+def upload_superid_call(src_img, TOKEN):
+    # Upload the image into PiktID's servers and get the id of the project and of the image
+    src_img_B = im_2_buffer(src_img)
+
+    options = '0'
+    m = MultipartEncoder(
+    fields={'options': options,
+            'file': ('file', src_img_B, 'text/plain')}
+    )
+    
+    response = requests.post(URL_API+'/upload_extra', 
+                            headers={
+                                      "Content-Type": m.content_type,
+                                      'Authorization': 'Bearer '+TOKEN},
+                            data=m,
+                            )
+
+    response_json = json.loads(response.text)
+    print(response_json)
+    
+    id_project = response_json.get('id_project')
+    id_image = response_json.get('id_image')
+
+    return id_project, id_image
+
+def upscaling_call(id_project, id_image, upscaling_parameters, TOKEN):
+
+    scale_factor = upscaling_parameters.scale_factor
+    upscaler_type = upscaling_parameters.upscaler_type
+    upscaling_mode = upscaling_parameters.upscaling_mode
+    output_format = upscaling_parameters.output_format
+    prompt = upscaling_parameters.prompt
+    controlnet_scale = upscaling_parameters.controlnet_scale
+    prompt_strength = upscaling_parameters.prompt_strength
+    num_inference_steps = upscaling_parameters.num_inference_steps
+
+    response = requests.post(URL_API+'/superid', 
+        headers={'Authorization': 'Bearer '+TOKEN},
+        json={'id_project':id_project, 'id_image':id_image, 'prompt': prompt, 'scale_factor':scale_factor, 'upscaler_type':upscaler_type, 'upscaling_mode':upscaling_mode, 'prompt_strength':prompt_strength, 'controlnet_conditioning_scale': controlnet_scale, 'num_inference_steps':num_inference_steps, 'output_format':output_format},
+        )
+
+    print(response.content)
+    response_json = json.loads(response.text)
+
+    return response_json
+
+# NOTIFICATION FUNCTIONS
+def get_notification_call(TOKEN):
+
+    response = requests.post(URL_API+'/notification_by_name', 
+        headers={'Authorization': 'Bearer '+TOKEN},
+        json={'name_list':'superid'}
+        )
+    print(f'response: {response.text}')
+    return
+
+def get_superid_info(id_project, id_image, upscaling_parameters, TOKEN):
+    # get ETA and credits needed for generation
+    scale_factor = upscaling_parameters.scale_factor
+    upscaling_mode = upscaling_parameters.upscaling_mode
+    strength = upscaling_parameters.prompt_strength
+    num_inference_steps = upscaling_parameters.num_inference_steps
+
+    response = requests.get(URL_API+'/superid_info', 
+        headers={'Authorization': 'Bearer '+TOKEN},
+        json={'id_project':id_project, 'id_image':id_image, 'scale_factor':scale_factor, 'upscaling_mode':upscaling_mode, 'strength':strength, 'num_inference_steps': num_inference_steps},
+    )
+    
+    #print(response.content) 
+    response_json = json.loads(response.text)
+    print(f'response: {response_json}')
+    eta = response_json.get('eta')
+    credits = response_json.get('required_credits')
+    width = response_json.get('width')
+    height = response_json.get('height')
+
+    return eta, credits, width, height
+
+def get_superid_link(id_project, id_image, TOKEN):
+    # get info on the upscaled image
+    response = requests.get(URL_API+'/superid', 
+        headers={'Authorization': 'Bearer '+TOKEN},
+        json={'id_project':id_project, 'id_image':id_image},
+    )
+    
+    #print(response.content) 
+    response_json = json.loads(response.text)
+    #print(f'response: {response_json}')
+    link = response_json[-1].get('l')
+    return link
