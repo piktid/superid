3077ebca85a0babf6b6907f233a9031dad8bd023
Nunzio Alexandro Letizia <alexletizia@Nunzios-MBP.station>
2024-12-30 15:21:09 +0100

    Update to v3.1

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 </p>
 
 
-# SuperID 3.0.1
+# SuperID 3.1.0
 [![Official Website](https://img.shields.io/badge/Official%20Website-piktid.com-blue?style=flat&logo=world&logoColor=white)](https://piktid.com)
 [![Discord Follow](https://dcbadge.vercel.app/api/server/FJU39e9Z4P?style=flat)](https://discord.com/invite/FJU39e9Z4P)
 
@@ -21,6 +21,7 @@ SuperID utilizes generative models to intelligently upscale your photos. It can 
 - <ins>Close-up portrait photos</ins>: Since the generative models behind SuperID are trained on human faces, the AI performs better when a single face is provided as input.
 - <ins>Small thumbnails</ins>: When the photo to upscale is at very low resolution (e.g. 128x128), SuperID can add missing details. 
 - <ins>Photos with noise and JPEG artifacts</ins>: SuperID can effectively denoise and remove artifacts from your photos.
+- <ins>Images with transparent backgrounds</ins>: SuperID intelligently upscale PNG images (e.g. logos) with transparent backgrounds.
 
 ## Getting Started
 <a target="_blank" href="https://colab.research.google.com/drive/1DBjyDcwrZBzFPFCDjRnmNHBt2mEqxW6D?usp=sharing">
@@ -58,24 +59,51 @@ url = 'your-url'
 $ python3 main.py
 ```
 
-Without any additional argument, SuperID utilizes the "super" method and provides the result asynchronously. 
-If you want to upscale x2, use the following general command:
+Without any additional argument, SuperID upscales the image times 2 and provides the result asynchronously. 
+If you want to change the output resolution, format or retrieval method, use the following general command:
 
 ```bash
-$ python3 main.py --upscaler_type '4' --scale_factor '2' --output_format 'PNG' --flag_email True --prompt '' --prompt_strength '0.35' --controlnet_scale '0.5' --num_inference_steps 20 --seed 0
+$ python3 main.py --scale_factor '2' --output_format 'PNG' --email
 ```
 
-- **upscaler_type**: Select which upscaler to use: 0 (None), 1 (Soft Portrait), 2 (Hard Portrait), 3 (Mix), 4 (Best overall)
-- **upscaling_mode**: Select the upscaling mode: fast (takes few seconds, lower quality, not available at the moment), super (takes up to minutes, higher quality overall)
 - **scale_factor**: Select the upscaling factor: upscale time 2 or times 4
 - **output_format**: Save the upscaled image in PNG or JPEG
-- **flag_email**: Get the output link via email once ready
+- **email**: Get the output link via email once ready
+
+
+## Face fixer
+It is now possible to automatically enhance little faces in photos while upscaling the input. To do so, add the flag:
+
+```bash
+$ python3 main.py --face_fixer
+```
+
+## Image denoising (coming soon)
+It is also possible to reduce the noise in the original photo by playing with the parameter (range 0-1), as in the example:
+
+```bash
+$ python3 main.py --denoise_input '0.5'
+```
+
+## Creative upscaling (for advanced users)
+To modify the capabilities of SuperID towards more conservative or creative outputs, control the upscaling process through the generative parameters as follows:
+
+```bash
+$ python3 main.py --prompt '' --prompt_strength '0.35' --controlnet_scale '0.5' --seed 0
+```
+
 - **prompt**: Describe your image
 - **prompt_strength**: The lower the more similar to the input image, the higher, the more diverse (range 0-1)
-- **guidance_scale**: How much to follow the description
+- **guidance_scale**: How much to follow the prompt description (range 1-10)
 - **controlnet_scale**: The higher, the more the upscaling will follow the lines of the input image (range 0-1)
-- **num_inference_steps**: The higher, the more denoising steps
 - **seed**: Choose a seed to replicate the results
+
+## Fast upscaling (coming soon)
+When the output quality is not the main priority, we allow users to opt for a fast upscaling process via the command:
+
+```bash
+$ python3 main.py --fast
+```
 
 ## Contact
 office@piktid.com
--- main.py
+++ main.py
@@ -13,19 +13,21 @@ if __name__ == '__main__':
     parser.add_argument('--url', help='Image file url', type=str, default='https://images.piktid.com/frontend/studio/superid/upscaler_sample/21z.png')
     parser.add_argument('--filepath', help='Input image file absolute path', type=str, default=None)
 
-    parser.add_argument('--upscaler_type', help='Select which upscaler to use: 0 (None), 1 (Soft Portrait), 2 (Hard Portrait), 3 (Mix), 4 (Best overall)', default='4')
-    parser.add_argument('--upscaling_mode', help='Select the upscaling mode: fast (takes few seconds, lower quality, currently not available), super (takes up to minutes, higher quality overall)', default='super')
+    parser.add_argument('--fast', help='Use the fast upscaler', action='store_true')
     parser.add_argument('--scale_factor', help='Select the upscaling factor: upscale time 2 or times 4', default='2')
-    parser.add_argument('--flag_email', help='Get the upscaled image via email', default=False)
+    parser.add_argument('--email', help='Get the upscaled image via email', action='store_true')
     parser.add_argument('--output_format', help='Save the upscaled image in PNG or JPEG', default='PNG')
 
     # Parameters only for the normal and super mode
-    parser.add_argument('--prompt', help='Describe your image', default=None)
-    parser.add_argument('--guidance_scale', help='Guidance scale', type=str, default=None)
-    parser.add_argument('--prompt_strength', help='Creativity, the lower the more similar to the input image, the higher, the more diverse (range 0-1)', type=str, default=None)
-    parser.add_argument('--controlnet_scale', help='Fidelity, the higher, the more the upscaling will follow the resemblance of the input image (range 0-1)', type=str, default=None)
-    parser.add_argument('--num_inference_steps', help='The higher, the more denoising steps', default=None)
-    parser.add_argument('--seed', help='Upscaling seed', type=int, default=randint(0,100000))
+    parser.add_argument('--prompt', help='Describe your image', default='')
+    parser.add_argument('--guidance_scale', help='Guidance scale', type=str, default='3')
+    parser.add_argument('--prompt_strength', help='Creativity, the lower the more similar to the input image, the higher, the more diverse (range 0-1)', type=str, default='0.35')
+    parser.add_argument('--controlnet_scale', help='Fidelity, the higher, the more the upscaling will follow the resemblance of the input image (range 0-1)', type=str, default='0.5')
+    parser.add_argument('--seed', help='Upscaling seed', type=int, default=randint(0, 100000))
+
+    # Pre-post processing parameters
+    parser.add_argument('--face_fixer', help='Fix small faces', action='store_true')
+    parser.add_argument('--denoise_input', help='Denoise filter amount, range 0-1', type=str, default='0.0')
 
     args = parser.parse_args()
 
@@ -34,18 +36,19 @@ if __name__ == '__main__':
     PASSWORD = os.getenv("SUPERID_PASSWORD")
 
     # Upscaling parameters
-    UPSCALER_TYPE = args.upscaler_type
-    UPSCALING_MODE = args.upscaling_mode
+    FAST_FLAG = args.fast
     SCALE_FACTOR = args.scale_factor
-    FLAG_EMAIL = args.flag_email
+    FLAG_EMAIL = args.email
     OUTPUT_FORMAT = args.output_format
 
     PROMPT = args.prompt
     GUIDANCE_SCALE = args.guidance_scale
     PROMPT_STRENGTH = args.prompt_strength
     CONTROLNET_SCALE = args.controlnet_scale
-    STEPS = args.num_inference_steps
     SEED = args.seed
+
+    FACE_FIXER = args.face_fixer
+    DENOISE_INPUT = args.denoise_input
 
     # Image parameters
     URL = args.url 
@@ -71,8 +74,6 @@ if __name__ == '__main__':
 
     PARAM_DICTIONARY = {
             'INPUT_PATH': INPUT_PATH,
-            'UPSCALER_TYPE': UPSCALER_TYPE,
-            'UPSCALING_MODE': UPSCALING_MODE,
             'SCALE_FACTOR': SCALE_FACTOR,
             'FLAG_EMAIL': FLAG_EMAIL,
             'OUTPUT_FORMAT': OUTPUT_FORMAT,
@@ -81,7 +82,9 @@ if __name__ == '__main__':
             'GUIDANCE_SCALE': GUIDANCE_SCALE,
             'PROMPT_STRENGTH': PROMPT_STRENGTH,
             'CONTROLNET_SCALE': CONTROLNET_SCALE,
-            'STEPS': STEPS,
+            'FACE_FIXER': FACE_FIXER,
+            'DENOISE_INPUT': DENOISE_INPUT,
+            'FAST': FAST_FLAG,
         }
 
     response = process_single_image(input_image, PARAM_DICTIONARY, TOKEN_DICTIONARY) 
